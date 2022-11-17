e7e3dd887cb53445adade25aa7327fbf69e6675a
Ivan Escribano <ivanescribano1998@gmail.com>
2022-11-17 13:49:44 +0100

    feat: home structure

---
+++ frontend/components/home/customerStories/CustomerStories.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomerStories = () => {
+  return <div>CustomerStories</div>;
+};
+
+export default CustomerStories;
---
+++ frontend/components/home/header/HeaderHome.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeaderHome = () => {
+  return <div>HeaderHome</div>;
+};
+
+export default HeaderHome;
