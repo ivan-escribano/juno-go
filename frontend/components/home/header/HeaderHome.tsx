8889c943cdd89698465b337f8eb61da912e98800
Ivan Escribano <ivanescribano1998@gmail.com>
2022-11-20 05:00:19 +0100

    feat:details styling header

--- frontend/components/home/header/HeaderHome.tsx
+++ frontend/components/home/header/HeaderHome.tsx
@@ -3,24 +3,26 @@ import Carousel from "./carousel/Carousel";
 
 const HeaderHome = () => {
   return (
-    <section className="flex h-full flex-col p-8">
+    <section className="flex h-full flex-col p-6">
       <h1 className="mb-5 px-5 text-center text-4xl font-semibold">
         Juno connects, activates and inspires.
       </h1>
       <Carousel />
-      <div className="">
-        <p className="my-5 text-center text-xl font-light">
+      <div>
+        <p className="font-regular my-8 text-center text-xl">
           Join our wonderful community where you will meet like-minded people
           and participate in different activities, according to your interests.
           Together we create a valuable everyday life!
         </p>
         <div className="my-5 flex justify-center">
-          <button className="rounded-md bg-blue-400 py-3 px-10 text-xl font-medium text-white">
+          <button className="main-bg rounded-lg py-3 px-10 text-xl font-medium text-white">
             Get started
           </button>
         </div>
         <div className="flex flex-col items-center">
-          <p>Download the app now and start today!</p>
+          <p className="my-3 text-center text-xl">
+            Download the app now and start today!
+          </p>
           <StoreLinks />
         </div>
       </div>
