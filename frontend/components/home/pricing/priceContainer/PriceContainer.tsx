b06654cde073d9bbdaba42b09ec32a38dc9d824f
Ivan Escribano <ivanescribano1998@gmail.com>
2022-11-20 05:29:39 +0100

    feat: pricing underline

--- frontend/components/home/pricing/priceContainer/PriceContainer.tsx
+++ frontend/components/home/pricing/priceContainer/PriceContainer.tsx
@@ -12,43 +12,49 @@ const PriceContainer = ({ price, bgColor, textColor }: Props) => {
     >
       <div>
         <div className="flex flex-col items-center">
-          <h3 className="text-5xl font-bold">{price} kr</h3>
+          <h3 className="relative text-5xl font-bold">
+            <span
+              className="main-bg absolute -left-1 top-1/2 h-1 rotate-12"
+              style={{ width: "115%" }}
+            ></span>
+            {price} kr
+          </h3>
           <p className=" my-2 text-2xl font-bold text-blue-500">Free</p>
           <p>Month</p>
         </div>
         <div className="mt-3">
           <div className="flex p-5">
-            <FaRegCheckCircle className="mr-5 flex-1 text-4xl" />
+            <FaRegCheckCircle className="main-text mr-5 flex-1 text-4xl" />
             <p style={{ flex: 11 }}>
               Take advantage of the entire Junoversum that offers you a valuable
               and active everyday life
             </p>
           </div>
           <div className="flex p-5">
-            <FaRegCheckCircle className="mr-5 flex-1 text-4xl" />
+            <FaRegCheckCircle className="main-text mr-5 flex-1 text-4xl" />
             <p style={{ flex: 11 }}>
               Take advantage of exclusive offers and inspiring articles from
               hand-picked partners and experts!
             </p>
           </div>
           <div className="flex p-5">
-            <FaRegCheckCircle className="mr-5 flex-1 text-4xl" />
+            <FaRegCheckCircle className="main-text mr-5 flex-1 text-4xl" />
             <p style={{ flex: 11 }}>
               Meet like-minded people in the Juno community, both in the app and
               on the website!
             </p>
           </div>
           <div className="flex p-5">
-            <FaRegCheckCircle className="mr-5  flex-1 text-4xl" />
+            <FaRegCheckCircle className="main-text  mr-5 flex-1 text-4xl" />
             <p style={{ flex: 11 }}>
               Get access to a marketplace tailored to your needs!
             </p>
           </div>
           <div className="flex p-5">
-            <FaRegCheckCircle className="mr-5 flex-1 text-4xl" />
+            <FaRegCheckCircle className="main-text mr-5 flex-1 text-4xl" />
             <p style={{ flex: 11 }}>Pay your membership monthly by card!</p>
           </div>
-          <div className="mt-5 flex justify-center">
+          <div className="mt-10 flex justify-center">
             <button className="rounded-lg border-2 border-blue-400 px-10 py-2">
               Sign up
             </button>
